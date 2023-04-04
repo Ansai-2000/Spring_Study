d9d4f22cddc585ee7d4fdeff40b78429061f6a77
jeongyounghun <dudgns3681@naver.com>
2023-04-04 19:58:43 +0900

    스프링 김영한 강의 클론 코딩

--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -22,6 +22,6 @@ public class OrderApp {
         memberService.join(member);
 
         Order order = orderService.createOrder(memberId,"itemA",10000);
-        System.out.println("orderss = " + order);
+        System.out.println("order = " + order);
     }
 }
