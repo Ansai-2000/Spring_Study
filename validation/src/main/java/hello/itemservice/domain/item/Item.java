aadda34c263ea407cf5486cb9ff7a5aaf3fbcd19
jeongyounghun <dudgns3681@naver.com>
2023-04-22 19:24:03 +0900

    김영한 스프링 강의 클론코딩

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -11,18 +11,18 @@ import javax.validation.constraints.NotNull;
 @Data
 public class Item {
 
-    @NotNull // 수정 요구사항 추가
+    @NotNull(groups = UpdateCheck.class) // 수정 요구사항 추가
     private Long id;
 
-    @NotBlank
+    @NotBlank(groups = {UpdateCheck.class,SaveCheck.class})
     private String itemName;
 
-    @NotNull
-    @Range(min = 1000,max=1000000)
+    @NotNull(groups = {UpdateCheck.class,SaveCheck.class})
+    @Range(min = 1000,max=1000000,groups = {UpdateCheck.class,SaveCheck.class})
     private Integer price;
 
-    @NotNull
-    //@Max(9999) // 수정 요구사항 추가
+    @NotNull(groups = {UpdateCheck.class,SaveCheck.class})
+    @Max(value=9999,groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
