ef02099594415ce3ad4dc22284d8d6a2b253e842
jeongyounghun <dudgns3681@naver.com>
2023-06-28 19:53:43 +0900

    김영한 스프링 강의 클론코딩

---
+++ jpashop2/src/main/java/jpabook/jpashop2/domain/DeliveryStatus.java
@@ -0,0 +1,7 @@
+package jpabook.jpashop2.domain;
+
+public enum DeliveryStatus {
+
+    READY,COMP
+
+}
---
+++ jpashop2/src/main/java/jpabook/jpashop2/domain/OrderStatus.java
@@ -0,0 +1,7 @@
+package jpabook.jpashop2.domain;
+
+public enum OrderStatus {
+
+   ORDER, CANCEL
+
+}
