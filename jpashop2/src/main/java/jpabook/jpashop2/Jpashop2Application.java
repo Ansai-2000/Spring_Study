5f22aeb041b9012844e3d0fef6eefcafbe9f32f5
jeongyounghun <dudgns3681@naver.com>
2023-06-30 18:46:25 +0900

    김영한 스프링 강의 클론코딩

--- jpashop2/src/main/java/jpabook/jpashop2/Jpashop2Application.java
+++ jpashop2/src/main/java/jpabook/jpashop2/Jpashop2Application.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop2;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -10,5 +12,11 @@ public class Jpashop2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Jpashop2Application.class, args);
 	}
+
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		return new Hibernate5Module();
+	}
+
 
 }
